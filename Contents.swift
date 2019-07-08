fa985a712a4054dc059e0ae7ebf574ab8a4e78e0
Anjali Sebastian <anjalisebastian@icloud.com>
2019-07-08 11:53:50 -0700

    First  commit

---
+++ Contents.swift
@@ -0,0 +1,26 @@
+//import UIKit
+////This is a comment to help remind you what your code does or other stuff like that.var
+//var str = "Hello, playground"
+//
+//var name = "Sabyatha"
+//var name2 = "Nailah"
+//
+//print("Hi \(name),  \(name2)! We are so glad you're here at KWK Portland")
+
+
+//Working with numbers. Integers and doubles
+
+//var int = 4
+//var double = 5.0
+//
+//print(Double(int) * double)
+
+
+
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+
+
